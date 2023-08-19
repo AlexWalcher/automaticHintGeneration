9e96c162ba07b27f52819f8db9a694e3d0147523
AlexWalcher <107101105+AlexWalcher@users.noreply.github.com>
2023-08-19 15:21:47 +0200

    Add files via upload

--- automaticHintGenerationWebapp.py
+++ automaticHintGenerationWebapp.py
@@ -1,7 +1,8 @@
 from my_modul import *
 
 import streamlit as st
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu 
+import option_menu
 from pathlib import Path
 import pandas as pd
 
