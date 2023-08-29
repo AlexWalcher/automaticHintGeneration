e8fc5ed256f137e7c3ff3bbe21eac49f4ca03d27
AlexWalcher <107101105+AlexWalcher@users.noreply.github.com>
2023-08-29 14:57:16 +0200

    Add files via upload

--- automaticHintGenerationWebapp.py
+++ automaticHintGenerationWebapp.py
@@ -13,7 +13,7 @@ with st.sidebar:
     selected = option_menu("Main Menu", ['Test', "Home", 'Upload file', 'Year question', 'Person question', 'Location question'],
         icons=['house', 'house', 'upload', '123', 'person', 'compass'], menu_icon="cast", default_index=0)
 
-if selected == "Test":
+if selected == "Tester":
     st.title('Automatic Hint Generation using Wikipedia')
     st.subheader('This WebApp takes a question, answer pair and returns a corresponding hint.')
 
@@ -26,18 +26,21 @@ if selected == "Test":
             st.session_state["preview"] += data[i]
     preview = st.text_area("CSV Preview", "", height=150, key="preview")
     upload_state = st.text_area("Upload State", "", key="upload_state")
+
     def upload():
         if uploaded_file is None:
             st.session_state["upload_state"] = "Upload a file first!"
         else:
             data = uploaded_file.getvalue().decode('utf-8')
-            parent_path = pathlib.Path(__file__).parent.parent.resolve()           
+            parent_path = pathlib.Path(__file__).parent.parent.resolve()
+            st.write(parent_path)           
             save_path = os.path.join(parent_path, "data")
             complete_name = os.path.join(save_path, uploaded_file.name)
             destination_file = open(complete_name, "w")
             destination_file.write(data)
             destination_file.close()
             st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+
     st.button("Upload file to Sandbox", on_click=upload)
 
 if selected == "Home":
